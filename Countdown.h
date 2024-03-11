69576319b36dfb2ced3eed8515cb8721f0d45f61
Leth4Lweap0N <leth4lweap0n@outlook.com>
2024-03-12 00:43:55 +0300

    Upload

---
+++ Countdown.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include <chrono>
+#include <condition_variable>
+#include <functional>
+#include <thread>
+#include <windows.h>
+
+class Countdown {
+
+public:
+	VOID SetTime(LONGLONG days, LONGLONG hours, LONGLONG minutes, LONGLONG seconds)
+	{
+		const auto total_hours = days * 24 + hours;
+		remaining_time_ = _STD chrono::hours(total_hours) + _STD chrono::minutes(minutes) + _STD chrono::seconds(seconds);
+	}
+
+	_STD chrono::seconds GetRemainingTime() const {
+		return remaining_time_;
+	}
+
+	LONGLONG GetDays() const {
+		return _STD chrono::duration_cast<_STD chrono::hours>(this->remaining_time_).count() / 24;
+	}
+
+	LONGLONG GetHours() const {
+		return _STD chrono::duration_cast<_STD chrono::hours>(this->remaining_time_).count() % 24;
+	}
+
+	LONGLONG GetMinutes() const {
+		return _STD chrono::duration_cast<_STD chrono::minutes>(this->remaining_time_ % _STD chrono::hours(1)).count();
+	}
+
+	LONGLONG GetSeconds() const {
+		return _STD chrono::duration_cast<_STD chrono::seconds>(this->remaining_time_ % _STD chrono::minutes(1)).count();
+	}
+
+	void StartTick(_STD function <void()> func = [] {}) {
+		_STD thread([this, func] {
+			while (remaining_time_.count() > 0) {
+				_STD this_thread::sleep_for(_STD chrono::seconds(1));
+
+
+				if (remaining_time_.count() > 0) {
+					remaining_time_ -= _STD chrono::seconds(1);
+				}
+			}
+				func();
+
+			}).detach();
+	}
+	explicit Countdown() : remaining_time_(0) {}
+private:
+	_STD chrono::seconds remaining_time_;
+};
---
+++ Entry.cpp
@@ -0,0 +1,30 @@
+// Entry.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include "Countdown.h"
+
+int main()
+{
+	Countdown countdown;
+	countdown.SetTime(0, 0, 0, 10);
+	countdown.StartTick([]
+		{
+			_STD cout << "Time's up!..b1tch" << '\n';
+			ExitProcess(EXIT_SUCCESS);
+		});
+	for (;; _STD this_thread::sleep_for(_STD chrono::seconds(1)))
+	{
+		_STD cout << "Main Thread..Running..." << '\n';
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
